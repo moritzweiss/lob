d0b044e6d7d12db9355d55e941045025af5d44a4
Moritz Weiss <moritz.weiss@math.ehtz.ch>
2024-03-21 20:47:16 +0100

    changes

---
+++ train/evaluate.py
@@ -0,0 +1,81 @@
+import numpy as np 
+import os 
+from os import environ
+import sys 
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+sys.path.append(current_dir)
+from ray import tune
+# N_THREADS = '1'
+# environ['OMP_NUM_THREADS'] = N_THREADS
+# environ['OPENBLAS_NUM_THREADS'] = N_THREADS
+# environ['MKL_NUM_THREADS'] = N_THREADS
+# environ['VECLIB_MAXIMUM_THREADS'] = N_THREADS
+# environ['NUMEXPR_NUM_THREADS'] = N_THREADS
+from simulation.all_markets_simulation import Market, config
+from ray.rllib.algorithms.ppo import PPOConfig
+import pandas as pd 
+
+env_config = config.copy()
+
+# restore best agent 
+path = f'{current_dir}/ray_results/40_noise'
+analysis = tune.ExperimentAnalysis(path, default_metric="episode_reward_mean", default_mode="max")
+best_trial = analysis.get_best_trial(metric="episode_reward_mean", mode="max", scope="last")
+config = analysis.get_best_config()
+path = analysis.get_best_checkpoint(trial=best_trial, mode="max", metric="episode_reward_mean")
+config['num_workers'] = 1
+# config['num_workers'] = 1
+AC = PPOConfig()
+AC.update_from_dict(config)
+agent = AC.build()
+agent.restore(path)    
+
+
+n = 5000
+# sample from market environment 
+results = {}
+rewards = []
+M = Market(config=env_config)
+for n in range(n):
+    # print(f'episode {n}')
+    observation, _ = M.reset()
+    terminated = False 
+    while not terminated:
+        # action = M.action_space.sample()
+        action = agent.compute_single_action(observation, explore=False)
+        # print(action)        
+        observation, reward, terminated, truncated, info = M.step(action)
+        # print(f'reward: {reward}')
+    rewards.append(info['total_reward'])
+results['rl_mean'] = np.mean(rewards)
+results['rl_std'] = np.std(rewards)
+
+
+# sample from market environment 
+print('------')
+rewards = []
+env_config['execution_agent'] = 'sl_agent'
+M = Market(config=env_config)
+for n in range(n):
+    # print(f'episode {n}')
+    observation, _ = M.reset()
+    terminated = False 
+    while not terminated:
+        # action = M.action_space.sample()
+        # action = agent.compute_single_action(observation)
+        # print(action)        
+        observation, reward, terminated, truncated, info = M.step(action=None)
+        # print(f'reward: {reward}')
+    rewards.append(info['total_reward'])
+results['sl_mean'] = np.mean(rewards)
+results['sl_std'] = np.std(rewards)
+
+print('#####')
+print(results)
+results = pd.DataFrame(results, index=[0])
+print(results)
+results.to_csv('latest_results.csv')
+
+
--- train/train.py
+++ train/train.py
@@ -1,23 +1,28 @@
 import os, sys 
 current_path = os.path.dirname(os.path.realpath(__file__))
 parent_dir = os.path.dirname(current_path)
+sys.path.append(parent_dir)
 from ray.rllib.algorithms.ppo import PPOConfig
 from ray import air 
-from advanced_multi_lot import Market 
 from ray import tune
 import copy 
 from ray.rllib.models import MODEL_DEFAULTS
 from gymnasium.utils.env_checker import check_env
+from simulation.all_markets_simulation import Market, config 
+import sys
 
 
-env_config = {'total_n_steps': int(1e3), 'log': False, 'seed': None, 'initial_level': 2, 'initial_volume': 10, 'env_type': 'down'}
+# ray.init(local_mode=args.local_mode)
+env_config = config.copy()
 M = Market(config=env_config)
 check_env(M)
 
+# custom config 
 custom_config = copy.deepcopy(MODEL_DEFAULTS)
 custom_config = {}
 custom_config['test'] = 1
 custom_config["vf_share_layers"] = False
+
 
 # this is for model configurations 
 model_config = { # By default, the MODEL_DEFAULTS dict above will be used.
@@ -27,28 +32,23 @@ model_config = { # By default, the MODEL_DEFAULTS dict above will be used.
 model_config = {"vf_share_layers": False} 
 
 
-config = (PPOConfig().rollouts(num_rollout_workers=17, batch_mode='complete_episodes', observation_filter='NoFilter')
+config = (PPOConfig().rollouts(num_rollout_workers=40, batch_mode='complete_episodes', observation_filter='NoFilter')
         .framework('torch')
         .resources(num_gpus=0)
-        .environment(env=Market, env_config=env_config)
-        .training(train_batch_size=1024, gamma=1.0, lr = 1e-3, _enable_learner_api=False, sgd_minibatch_size=256 , num_sgd_iter=1 , use_kl_loss=False , clip_param=tune.grid_search([0.3, 0.5, 1.0, 3.0]) , vf_clip_param=100.0, use_gae=False, use_critic=True, vf_loss_coeff=1.0, model=model_config)
+        .environment(env=Market, env_config=config)
+        # .training(train_batch_size=1024, gamma=1.0, lr = 1e-3, sgd_minibatch_size=256 , num_sgd_iter=1 , use_kl_loss=False , clip_param=tune.grid_search([0.3, 0.5, 1.0, 3.0]) , vf_clip_param=100.0, use_gae=False, use_critic=True, vf_loss_coeff=1.0, model=model_config)
+        .training(train_batch_size=1024, gamma=1.0, lr = 1e-3, sgd_minibatch_size=256 , num_sgd_iter=2 , use_kl_loss=False , clip_param=tune.grid_search([1.0]) , vf_clip_param=100.0, use_gae=False, use_critic=True, vf_loss_coeff=1.0, model=model_config)
         .debugging(fake_sampler=False)
-        .environment(disable_env_checking=False)
-        .rl_module(_enable_rl_module_api=False)
+        .environment(disable_env_checking=True)
         .reporting(metrics_num_episodes_for_smoothing=500)
+        # .rl_module(_enable_rl_module_api=False)
         ) 
 
-# algo = config.build()
-# algo.train()
-# name = f"{env_config['initial_volume']}_{env_config['env_type']}"
-# path = f"{current}/results"
-# verbose = 0 means silent 
-# verbose = 1  means status updates
 
 tuner = tune.Tuner(
     "PPO",
     run_config=air.RunConfig(
-        stop={"training_iteration": 50}, storage_path = f"{current_path}/results",  name = f"{env_config['initial_volume']}_{env_config['env_type']}", 
+        stop={"training_iteration": 100}, storage_path = f"{parent_dir}/ray_results",  name = f"{env_config['volume']}_{env_config['type']}", 
         checkpoint_config=air.CheckpointConfig(checkpoint_frequency=5, checkpoint_at_end=True, checkpoint_score_order='max', 
                                                checkpoint_score_attribute='episode_reward_mean'), verbose=1,
     ),
@@ -57,21 +57,3 @@ tuner = tune.Tuner(
 
 
 results = tuner.fit()
-
-
-
-# Get the best result based on a particular metric.
-# best_result = results.get_best_result(metric="episode_reward_mean", mode="max", scope='all')
-
-# Get the best checkpoint corresponding to the best result.
-# best_checkpoint = best_result.checkpoint
-
-# from ray.rllib.algorithms.algorithm import Algorithm
-# algo = Algorithm.from_checkpoint(best_checkpoint.path)
-
-
-
-
-
-
-
