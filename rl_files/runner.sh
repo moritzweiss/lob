8a22c6cbb03876c05f6fa7635410d6b546a64bb7
Moritz Weiss <moritz.weiss@math.ethz.ch>
2025-06-11 22:02:58 +0200

    update runner for qf branch

--- rl_files/runner.sh
+++ rl_files/runner.sh
@@ -22,9 +22,11 @@ declare -a ARGS=(
   "strategic 60"
 )
 
-NUM_STEPS=100
-NUM_ENVS=128 
-NUM_ITERATIONS=400
+NUM_STEPS=50
+# NUM_ENVS=128 
+NUM_ENVS=2
+# NUM_ITERATIONS=400
+NUM_ITERATIONS=3
 TIMESTEPS=$((NUM_ITERATIONS * $NUM_ENVS * $NUM_STEPS))
 echo "time steps: "
 echo $TIMESTEPS
@@ -35,12 +37,13 @@ echo $NUM_ENVS
 echo "num iterations: "
 echo $NUM_ITERATIONS
 
-NUM_EVALUATION_EPISODES=10000
-# NUM_EVALUATION_EPISODES=10
+# NUM_EVALUATION_EPISODES=10000
+NUM_EVALUATION_EPISODES=10
 echo "num evaluation episodes: "
 echo $NUM_EVALUATION_EPISODES
 
-for exp_name in "log_normal" "dirichlet"; 
+# for exp_name in "log_normal" "dirichlet"; 
+for exp_name in "log_normal" 
 # normal distribution performs more or less the same as dirichlet. therefore we are ignoring the normal distribution for the moment 
 do
 for args in "${ARGS[@]}"; 
@@ -53,7 +56,7 @@ do
   echo "#####" 
   echo "STARTING A RUN"  
   echo "Running experiment: $exp_name with $ARG1 $ARG2"
-  python3 "$PYTHON_SCRIPT" --env_type "$ARG1" --num_lots "$ARG2" --total_timesteps "$((TIMESTEPS))" --num_envs "$((NUM_ENVS))" --num_steps "$((NUM_STEPS))" --n_eval_episodes "$((NUM_EVALUATION_EPISODES))" --exp_name "$exp_name" 
+  python3 "$PYTHON_SCRIPT" --env_type "$ARG1" --num_lots "$ARG2" --total_timesteps "$((TIMESTEPS))" --num_envs "$((NUM_ENVS))" --num_steps "$((NUM_STEPS))" --n_eval_episodes "$((NUM_EVALUATION_EPISODES))" --exp_name "$exp_name" --tag "qfunction"
 done
 done 
 
